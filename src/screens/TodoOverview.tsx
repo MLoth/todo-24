6f3416a2857c6efad93512ddfb915d0f67350a51
Marty <martijn.loth@icloud.com>
2024-03-06 11:25:05 +0100

    refactor: moved new todo functionality to component

--- src/screens/TodoOverview.tsx
+++ src/screens/TodoOverview.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { uid } from 'uid'
-import { ChevronDown, Plus, Settings } from 'lucide-react'
+import { Settings } from 'lucide-react'
 
 import { Todo } from '../models/Todo'
 import { AppFooter } from '../components/AppFooter'
@@ -9,29 +8,13 @@ import { AppHeader } from '../components/AppHeader'
 import TodoItem from '../components/TodoItem'
 
 export const TodoOverview = () => {
-  const emptyTodo: Todo = {
-    task: '',
-    category: 'choose',
-    isCompleted: false,
-  }
   // TODO: remove item from list when checked (delayed by 3 seconds)
 
   // TODO: release better version (v1.1.0)
-  const [isValid, setIsValid] = useState({
-    task: {
-      dirty: false, // Has the user interacted with the input field?
-      valid: false,
-    },
-    category: {
-      dirty: false,
-      valid: false,
-    },
-  })
 
   const [todos, setTodos] = useState<Todo[]>(
     localStorage.todos ? JSON.parse(localStorage.todos) : [],
   )
-  const [newTodo, setNewTodo] = useState<Todo>(emptyTodo)
 
   useEffect(() => {
     localStorage.todos = JSON.stringify(todos)
@@ -52,19 +35,6 @@ export const TodoOverview = () => {
     setTodos(updatedTodos)
   }
 
-  const addNewTodo = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault() // Stop posting naar zelfde pagina
-
-    if (newTodo.task === '' || newTodo.category === 'choose') return // TODO: show error message
-
-    setNewTodo(() => {
-      const currentNewTodo = { ...newTodo, id: uid() }
-      setTodos([...todos, currentNewTodo]) // Combineer de huidige todos met de nieuwe todo
-      // This might confuse some developers.
-      return emptyTodo
-    }) // Maak een unieke id aan voor het opslaan van deze nieuwe todo
-  }
-
   return (
     <div className="flex flex-col min-h-screen mx-auto max-w-2xl px-6">
       {/* Header: amount of todo & welcome message */}
@@ -82,102 +52,7 @@ export const TodoOverview = () => {
       </div>
 
       <div className="flex-1">
-        <form
-          className="flex gap-6 bg-white shadow py-3 px-6 rounded-2xl mb-6"
-          onSubmit={addNewTodo}
-        >
-          <div className="flex items-center">
-            <button
-              className="h-auto rounded-full p-2 border border-neutral-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:border-transparent hover:text-white hover:bg-blue-500 disabled:opacity-10 disabled:cursor-not-allowed"
-              disabled={!isValid.task.valid || !isValid.category.valid}
-            >
-              <Plus className="stroke-current" />
-              <span className="sr-only">Add todo</span>
-            </button>
-          </div>
-
-          <div className="w-full">
-            <input
-              className={`block w-full border border-neutral-200 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent mb-1 text-lg ${
-                !isValid.task.valid && isValid.task.dirty
-                  ? 'border-red-500 placeholder:text-red-300 focus:ring-red-700'
-                  : ''
-              }`}
-              placeholder="Add a new todo..."
-              type="text"
-              name="new-todo"
-              id="new-todo"
-              value={newTodo.task}
-              onInput={(event: React.FormEvent<HTMLInputElement>) => {
-                setIsValid({
-                  ...isValid,
-                  task: {
-                    dirty: true,
-                    valid: event.currentTarget.value.length > 0,
-                  },
-                })
-                setNewTodo({ ...newTodo, task: event.currentTarget.value })
-              }}
-              onBlur={(event: React.FormEvent<HTMLInputElement>) => {
-                setIsValid({
-                  ...isValid,
-                  task: {
-                    dirty: true,
-                    valid: event.currentTarget.value.length > 0,
-                  },
-                })
-              }}
-            />
-            <div className="flex justify-between items-center relative w-fit">
-              <select
-                className={`appearance-none w-full py-1 px-3 text-sm border pr-7 border-neutral-200 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent font-semibold placeholder:text-neutral-400 ${
-                  !isValid.category.valid && isValid.category.dirty
-                    ? 'border-red-500 placeholder:text-red-500 text-red-500 focus:ring-red-700'
-                    : ''
-                }`}
-                name="category"
-                id="category"
-                value={newTodo.category}
-                onChange={(event: React.FormEvent<HTMLSelectElement>) => {
-                  setIsValid({
-                    ...isValid,
-                    category: {
-                      dirty: true,
-                      valid: event.currentTarget.value !== 'choose',
-                    },
-                  })
-                  setNewTodo({
-                    ...newTodo,
-                    category: event.currentTarget.value,
-                  })
-                }}
-                onBlur={(event: React.FormEvent<HTMLSelectElement>) => {
-                  setIsValid({
-                    ...isValid,
-                    category: {
-                      dirty: true,
-                      valid: event.currentTarget.value !== 'choose',
-                    },
-                  })
-                }}
-              >
-                <option disabled value={'choose'}>
-                  Choose a category.
-                </option>
-                <option value="hobby">Hobby</option>
-                <option value="work">Work</option>
-                <option value="nevenactiviteiten">Nevenactiviteiten</option>
-              </select>
-              <ChevronDown
-                className={`absolute right-0 mr-1 pointer-events-none stroke-current text-neutral-400 ${
-                  !isValid.category.valid && isValid.category.dirty
-                    ? 'text-red-500'
-                    : ''
-                }`}
-              />
-            </div>
-          </div>
-        </form>
+        {/* TODO: move to TodoInput.tsx */}
 
         <div className="flex flex-col gap-3">
           {todos.map((todo: Todo) => (
@@ -189,7 +64,6 @@ export const TodoOverview = () => {
             />
           ))}
         </div>
-        {/* TODO: show message when there a no todos */}
       </div>
 
       {/* Footer: about the app (c) Martijn - CURRENT YEAR */}
